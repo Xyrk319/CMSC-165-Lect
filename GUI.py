2f4bf90dc9dda951d85e629d96438a19258c3acb
Elcid Cruzado <elcid.cruzado@dashlabs.ai>
2022-12-09 20:01:39 +0800

    feat: UI refactor with customtkinter UI-library

--- GUI.py
+++ GUI.py
@@ -1,55 +1,52 @@
-from tkinter import filedialog
 from tkinter import *
-from tkinter import ttk
+from customtkinter import *
 from PIL import ImageTk, Image
 from ImageProcessor import ImageProcessor
 
 
 class GUI:
-
     def __init__(self):
-
+        set_appearance_mode("dark")
+        set_default_color_theme("green")
         # attributes here
-        self.window = Tk()
-        self.window.title("CMSC 165 - Lecture Project")
-        self.window.geometry("1200x780")
+        # self.window = Tk()
+        self.window = CTk()
         self.image_processor = None
-
         self.image_path = None
 
-        # File opener
-        self.file_opened = False
-
-        # File Explorer
-        self.file_frame = Frame(self.window, width=450,
-                                height=50, relief="sunken", background="black")
-        self.file_frame.place(x=10, y=15)
-        self.file_btn = Button(self.file_frame, text="Open File",
-                               command=self.fileOpener, pady=5).pack()
-
-        # count pollens
-        # frame
-        self.button_frame = Frame(self.window)
-
-        self.button_frame.place(x=10, y=50)
+        # frames
+        self.image_frame = CTkFrame(self.window, width=650, height=400)
+        self.sidebar_frame = CTkFrame(self.window, width=200, corner_radius=0)
 
         # buttons
-        self.count_light_pollen = Button(
-            self.button_frame, text="Count Light Pollen", command=self.countLight, padx=5, pady=5).pack(pady=10)
-        self.count_dark_pollen = Button(
-            self.button_frame, text="Count Dark Pollen", command=self.countDark, padx=5, pady=5).pack(pady=10)
+        self.file_btn = CTkButton(self.sidebar_frame, text="Open File", command=self.fileOpener)
+        self.count_light_pollen = CTkButton(self.sidebar_frame, text="Count Light Pollen", command=self.countLight)
+        self.count_dark_pollen = CTkButton(self.sidebar_frame, text="Count Dark Pollen", command=self.countLight)
 
-        # textbox
+        # File opener
+        # self.file_opened = False
 
-        # images
+    def draw(self):
+        self.window.title("CMSC 165 - Lecture Project")
+        # self.window.geometry("1200x780")
 
-        self.image_frame = Frame(self.window, width=650,
-                                 height=500, relief="sunken", background="white")
-        self.image_frame.place(x=500, y=15)
-        self.img = None
+        # component frames
+        # self.file_frame = Frame(self.window, width=450, height=50, relief="sunken", background="black")
+        # self.img = None
+        # self.button_frame = Frame(self.window)
 
-        # MAIN LOOP
-        self.window.mainloop()
+        # buttons
+
+        self.image_frame.grid(row=0, column=1, columnspan=4, padx=20, pady=20)
+        self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nesw")
+        self.file_btn.grid(sticky="w", row=0, column=0, padx=20, pady=10)
+        self.count_light_pollen.grid(sticky="w", row=1, column=0, padx=20, pady=10)
+        self.count_dark_pollen.grid(sticky="w", row=2, column=0, padx=20, pady=10)
+
+        # self.file_frame.place(x=10, y=15)
+        # self.button_frame.place(x=10, y=50)
+        # frame
+        # count pollens
 
     def loadImageProcessor(self, path):
         self.image_processor = ImageProcessor(path)
@@ -58,8 +55,11 @@ class GUI:
 
         # open a file explorer
         # get the file name of the selected file by the user
-        filename = filedialog.askopenfilename(initialdir="./", title="Select a File", filetypes=(
-            ("JPG Files", "*.jpg"), ("JPEG Files", "*.jpeg"), ("PNG Files", "*.png")))
+        filename = filedialog.askopenfilename(
+            initialdir="./assets/",
+            title="Select a File",
+            filetypes=(("JPG Files", "*.jpg"), ("JPEG Files", "*.jpeg"), ("PNG Files", "*.png")),
+        )
 
         # if the file open is succesful
         if filename:
@@ -67,6 +67,7 @@ class GUI:
             self.image_path = filename
             self.loadImageProcessor(filename)
             self.displayImage()
+        # pass
 
     def countLight(self):
         pass
@@ -78,4 +79,4 @@ class GUI:
         im = Image.fromarray(self.image_processor.display_img)
         self.img = ImageTk.PhotoImage(image=im)
         label = Label(self.image_frame, image=self.img)
-        label.pack()
+        label.grid(row=0, column=0, padx=10, pady=10, sticky="nesw")
--- main.py
+++ main.py
@@ -2,5 +2,7 @@ from GUI import GUI
 from ImageProcessor import ImageProcessor
 
 run = GUI()
+run.draw()
+run.window.mainloop()
 
 # imageProcessor = ImageProcessor('assets/practice_image_1.jpg')
